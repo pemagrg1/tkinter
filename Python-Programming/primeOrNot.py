fa11d8bdba06950667f6f74d074069315fd185dd
pemagrg1 <pema.grg1@gmail.com>
2022-03-29 10:28:36 +0545

    simple python programming

---
+++ Python-Programming/primeOrNot.py
@@ -0,0 +1,11 @@
+# Check if given number is prime or not. Ex Prime numbers: 2, 3, 5, 7, 13
+i, temp = 0, 0
+n = int(input("please give a number : "))
+for i in range(2, n//2):
+    if n % i == 0:
+        temp = 1
+        break
+if temp == 1:
+    print("given number is not prime")
+else:
+    print("given number is prime")
